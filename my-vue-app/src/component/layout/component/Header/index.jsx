63127424d7f6c09b79394a0176a736c8e1d3235d
Son Do <dohongson271298@gmail.com>
2024-01-09 18:05:01 +0700

    update feature search

--- my-vue-app/src/component/layout/component/Header/index.jsx
+++ my-vue-app/src/component/layout/component/Header/index.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import Tippy from '@tippyjs/react';
 
+import { Wrapper as PopperWrapper } from '../../../../component/Popper'
 import styles from './Header.module.scss'
 import images from '../../../../assets/images'
 
@@ -11,6 +13,14 @@ const cx = classNames.bind(styles)
 // console.log(images.logo)
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3])
+        }, 3000);
+    }, [])
+    
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -18,20 +28,30 @@ function Header() {
                     <img src={images.logo} alt="tiktok" />
                 </div>
 
-                <div className={cx('search')}>
-                    <input placeholder='Search accounts and videos' spellCheck={false}/>
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
+                <Tippy
+                    interactive={true}
+                    visible={searchResult.length > 0}
+                    render={attrs => (
+                        <PopperWrapper>
+                            <div className='box' tabIndex='-1' {...attrs}>
+                                hihihhi
+                            </div>
+                        </PopperWrapper>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder='Search accounts and videos' spellCheck={false}/>
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
 
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
-                    <Tippy content='Tìm kiếm'>
                         <button className={cx('search-btn')}>
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
-                    </Tippy>
-                </div>
+                    </div>
+                </Tippy>
 
                 <div className={cx('actions')}>
                     <button>Upload</button>
